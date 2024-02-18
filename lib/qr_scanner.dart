021fa80848051685287c18102c75d2616ffa8496
acrux <acrux@cipherbiz.com>
2024-02-18 23:34:29 +0300

    inital

---
+++ lib/qr_scanner.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:qr_code_project/result_screen.dart';
+
+const bgColor = Color(0xfffafafa);
+
+class QRScanner extends StatefulWidget {
+  const QRScanner({super.key});
+
+  @override
+  State<QRScanner> createState() => _QRScannerState();
+}
+
+class _QRScannerState extends State<QRScanner> {
+  bool isScanCompleted = false;
+
+  void closeScreen() {
+    isScanCompleted = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "QR Scanner",
+          style: TextStyle(
+            color: Colors.black87,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1,
+          ),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Place the qr code in the area",
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Scan will be stareted automatically",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: MobileScanner(
+                  allowDuplicates: true,
+                  onDetect: (barcode, args) {
+                    if (!isScanCompleted) {
+                      String code = barcode.rawValue ?? '---';
+                      isScanCompleted = true;
+                      Navigator.push(context,MaterialPageRoute(builder: (context)=>  ResultScreen(closeScreen: closeScreen,code: code,)));
+                    }
+                  }),
+            ),
+            Expanded(
+                child: Container(
+              alignment: Alignment.center,
+              child: const Text(
+                "devolodep mhamut ",
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 14,
+                  letterSpacing: 1,
+                ),
+              ),
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
